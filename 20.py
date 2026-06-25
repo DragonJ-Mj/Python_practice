d869f29292c95bf185996a9862385785c41b8395
DragonJMj <imdragonmj@gmail.com>
2026-06-25 11:40:13 +0530

    21.py added

---
+++ 20.py
@@ -0,0 +1,10 @@
+# use a for loop to find and print the largest number in a list.
+# ​Given: scores = [45, 82, 94, 12, 75, 63]
+
+
+scores = [45, 82, 94, 12, 75, 63]
+largest = scores[0]
+for i in scores:
+    if i > largest:
+        largest = i
+print(largest)

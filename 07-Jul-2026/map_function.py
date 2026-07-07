f38d5cf69618557f0f087e72f4abfe25327b5ca8
DragonJMj <imdragonmj@gmail.com>
2026-07-07 12:01:27 +0530

    updated

--- 07-Jul-2026/map_function.py
+++ 07-Jul-2026/map_function.py
@@ -8,12 +8,7 @@ sq= list(map(lambda x : x**2, number))
 print(sq)
 
 
-even number
-```py
+##even number
 
 even_num = list(map(lambda x: x%2==0, [1,2,3,4,5,6,7,8,9]))
 print(even_num)
-'''output
-[False, True, False, True, False, True, False, True, False]
-'''
-```
